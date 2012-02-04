2c2c4c32e742942fef713067b179129445fe6397
Stephan Michels <stephan.michels@gmail.com>
2012-02-04 01:22:42 +0100

    Using template images.

--- InspectorTabBar/SMAppDelegate.m
+++ InspectorTabBar/SMAppDelegate.m
@@ -30,23 +30,33 @@
     // Insert code here to initialize your application
     NSMutableArray *newItems = [NSMutableArray arrayWithCapacity:5];
     {
-        SMTabBarItem *item = [[SMTabBarItem alloc] initWithImage:[NSImage imageNamed:@"Image1.png"] tag:0];
+        NSImage *image = [NSImage imageNamed:@"Image1.png"];
+        image.template = YES;
+        SMTabBarItem *item = [[SMTabBarItem alloc] initWithImage:image tag:0];
         [newItems addObject:item];
     }
     {
-        SMTabBarItem *item = [[SMTabBarItem alloc] initWithImage:[NSImage imageNamed:@"Image2.png"] tag:1];
+        NSImage *image = [NSImage imageNamed:@"Image2.png"];
+        image.template = YES;
+        SMTabBarItem *item = [[SMTabBarItem alloc] initWithImage:image tag:1];
         [newItems addObject:item];
     }
     {
-        SMTabBarItem *item = [[SMTabBarItem alloc] initWithImage:[NSImage imageNamed:@"Image3.png"] tag:2];
+        NSImage *image = [NSImage imageNamed:@"Image3.png"];
+        image.template = YES;
+        SMTabBarItem *item = [[SMTabBarItem alloc] initWithImage:image tag:2];
         [newItems addObject:item];
     }
     {
-        SMTabBarItem *item = [[SMTabBarItem alloc] initWithImage:[NSImage imageNamed:@"Image4.png"] tag:3];
+        NSImage *image = [NSImage imageNamed:@"Image4.png"];
+        image.template = YES;
+        SMTabBarItem *item = [[SMTabBarItem alloc] initWithImage:image tag:3];
         [newItems addObject:item];
     }
     {
-        SMTabBarItem *item = [[SMTabBarItem alloc] initWithImage:[NSImage imageNamed:@"Image5.png"] tag:4];
+        NSImage *image = [NSImage imageNamed:@"Image5.png"];
+        image.template = YES;
+        SMTabBarItem *item = [[SMTabBarItem alloc] initWithImage:image tag:4];
         [newItems addObject:item];
     }
     self.tabBar.items = newItems;
